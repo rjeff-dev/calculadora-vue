e0a42c180860a9a1a1c72ebe24cb9fac9276a6de
rjeff-dev <jeff_xd00@hotmail.com>
2025-12-08 14:12:39 -0300

    1° tentativa

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,31 @@
 <script setup>
 import { reactive } from 'vue';
 
-const calcularota  = reactive({
-
+const calc  = reactive({
+    n1: '',
+    n2: '',
+    resultado: ''
 })
+
+//<=================================Funções para a calculadora=======================================>
+function somar() {
+    calc.resultado = Number(calc.n1) + Number(calc.n2);
+}
+
+function sub() {
+    calc.resultado = Number(calc.n1) - Number(calc.n2);
+}
+
+function mult() {
+    calc.resultado = Number(calc.n1) * Number(calc.n2);
+}
+
+function div() {
+    calc.resultado = 
+        calc.n2 === 0
+            ?"Erro: divição por zero"
+            : Number(calc.n1) / Number(calc.n2);
+}
 
 </script>
 
@@ -16,31 +38,33 @@ const calcularota  = reactive({
             <div class="d-flex justify-content-between">
                 <div class="col-5">
                     <label class="form-label" for="">Digite o primeiro número:</label>
-                    <input type="number" class="form-control">
+                    <input type="number" class="form-control" v-model="calc.n1">
                 </div>
                 <div class="col-5">
                     <label class="form-label" for="">Digite o segundo número:</label>
-                    <input type="number" class="form-control">
+                    <input type="number" class="form-control" v-model="calc.n2">
                 </div>
             </div>
-            <form>
+            <form @submit.prevent>
                 <div class="row">
                     <div :class="['col-6', 'mt-5', 'd-flex', 'justify-content-center']">
-                        <button type="button" class="btn btn-primary">Soma +</button>
+                        <button @click.prevent="somar" type="submit" class="btn btn-primary">Soma +</button>
                     </div>
                     <div :class="['col-6', 'mt-5', 'd-flex', 'justify-content-center']">
-                        <button type="button" class="btn btn-primary">Divisão ÷</button>
+                        <button @click.prevent="div" type="submit" class="btn btn-primary">Divisão ÷</button>
                     </div>
                     <div :class="['col-6', 'mt-5', 'd-flex', 'justify-content-center']">
-                        <button type="button" class="btn btn-primary">Subtração -</button>
+                        <button @click.prevent="sub" type="submit" class="btn btn-primary">Subtração -</button>
                     </div>
                     <div :class="['col-6', 'mt-5', 'd-flex', 'justify-content-center']">
-                        <button type="button" class="btn btn-primary">Multplicação ×</button>
+                        <button @click.prevent="mult" type="submit" class="btn btn-primary">Multplicação ×</button>
                     </div>
                 </div>
                 <div class="row">
                     <div :class="['col', 'mt-5', 'd-flex', 'justify-content-center']">
-                        <button type="button" :class="['btn', 'btn-success']">Calcular</button>
+                        <p :class="['operacao']">
+                            Resultado: {{ calc.resultado }}
+                        </p>
                     </div>
                 </div>
             </form>
@@ -65,5 +89,8 @@ const calcularota  = reactive({
     }
     .header-bc{
         background-color: #8c4ef7;
+    }
+    .operacao{
+        font-size: 24px;
     }
 </style>
